3491d1c35951952622d6ff75e0696bb1a28f0aa7
Hector Toro <alejotoro94@hotmail.com>
2024-03-14 20:23:11 -0500

    Se usa un componente personalizado de primeNG, en este caso menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -12,8 +14,10 @@ import { ButtonModule } from 'primeng/button';
   ],
   imports: [
     AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
     ButtonModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
