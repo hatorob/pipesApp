4c0bbd6d21819c1608150e304262f1b116b3b456
Hector Toro <alejotoro94@hotmail.com>
2024-03-16 16:21:06 -0500

    Se agrega menubar de primeng para nuestra aplicación

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'shared-menu',
@@ -13,13 +14,34 @@ export class MenuComponent {
     ngOnInit() {
         this.menuItems = [
             {
-                label: 'New',
-                icon: 'pi pi-fw pi-plus',
+                label: 'Pipes de Angular',
+                icon: 'pi pi-desktop',
+                items: [
+                  {
+                    label: 'Textos y Fechas',
+                    icon: 'pi pi-align-left'
+                  },
+                  {
+                    label: 'Números',
+                    icon: 'pi pi-dollar'
+                  },
+                  {
+                    label: 'No comunes',
+                    icon: 'pi pi-globe',
+                  }
+                ]
             },
             {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-trash'
+              label: 'Pipes Personalizados',
+              icon: 'pi pi-cog',
+              items: [
+                {
+                  label: 'Otro elemento',
+                  icon: 'pi pi-cog',
+                }
+              ]
             }
+
         ];
     }
 
