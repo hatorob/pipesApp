c9a0876cda4b61879de18d488219f287ea653bec
Hector Toro <alejotoro94@hotmail.com>
2024-03-17 14:27:58 -0500

    terminada la clase

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -53,4 +53,6 @@ export class OrderComponent {
     this.sortByType = value;
   }
 
+
+
 }
--- src/app/products/pipes/sort-by.pipe.ts
+++ src/app/products/pipes/sort-by.pipe.ts
@@ -20,7 +20,6 @@ export class SortByPipe implements PipeTransform {
       default:
         return heroes;
     }
-    return heroes;
   }
 
 }
