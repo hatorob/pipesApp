1f25d64e5edd2f1b0af1d72f6c609784696c5c1b
Hector Toro <alejotoro94@hotmail.com>
2024-03-17 11:52:10 -0500

    Usamos el pipe de async tanto en observables como en promesas

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -43,6 +44,18 @@ export class UncommonPageComponent {
    age: 36,
    address: 'Ottawa, Canada'
   };
+
+  // Async pipe
+
+  public myObservableTimer = interval(2000).pipe(
+    tap( value => console.log("value", value))
+  );
+
+  public promiseValue: Promise<string> = new Promise( (resolve, reject) => {
+    setTimeout( () => {
+      resolve("Tenemos data en la promesa")
+    }, 3500);
+  });
 
 
 }
